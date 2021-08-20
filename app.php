90bea1118c30dc93f9b0710eda07df4be01ffb16
Ferdanur ARMUTLU <ferdanurarmutlu@gmail.com>
2021-08-20 23:31:03 +0300

    Update app.php
    İlk ödev yapıldı.

--- app.php
+++ app.php
@@ -18,3 +18,16 @@ $age = 25;
  * 
  * İyi çalışmalar.
  */
+if ($age >= "0" && $age < "15") {
+    // koşul1 doğru ise çalışacak olan kısım
+    echo("Çocuk");
+} else if ($age >= "15" && $age <"25") {
+    // koşul1 yanlış ve koşul2 doğru ise çalışacak
+    echo("Genç");
+} else if ($age >= "25" && $age < "65") {
+    // koşul1, koşul2 yanlış ve koşul3 doğru ise çalışacak
+    echo("Yetişkin");
+} else {
+    // hiçbir koşul doğru değilse çalışacak
+    echo("Yaşlı");
+}
